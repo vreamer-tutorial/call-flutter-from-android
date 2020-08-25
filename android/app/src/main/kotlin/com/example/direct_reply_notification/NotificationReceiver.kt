e105bcb0ea1253e7ff58aa25bffe918b6fd809d9
Vreamer <yhou.vreamer@gmail.com>
2020-08-25 10:52:13 +0800

    Capture user input

--- android/app/src/main/kotlin/com/example/direct_reply_notification/NotificationReceiver.kt
+++ android/app/src/main/kotlin/com/example/direct_reply_notification/NotificationReceiver.kt
@@ -1,4 +1,19 @@
 package com.example.direct_reply_notification
 
-class NotificationReceiver {
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.app.RemoteInput
+
+class NotificationReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val remoteInput = RemoteInput.getResultsFromIntent(intent)
+
+        if (remoteInput != null) {
+            val title = remoteInput.getCharSequence(
+                    NotificationHelper.KEY_TEXT_REPLY).toString()
+            Log.d("NotificationReceiver", title)
+        }
+    }
 }
